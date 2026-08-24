176a8fc0d0d64d589d59e257076dd47017c4cee4
Muhammad Mahad <mahad.pk111@gmail.com>
2026-08-24 09:46:16 +0500

    Added Query Selector method
    Before the program was highly dependent on browser. Now it still dependent but not that much.

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,161 +1,158 @@
 function resultCardGeneration(event) {
   event.preventDefault();
-  // Student information object
 
+  // Student Info Object
   let student = {
-    rollNumber: rollNumber.value,
-    fullName: fullName.value,
-    fatherName: fatherName.value,
-    studentClass: studentClass.value,
+    rollNumber: document.querySelector("#rollNumber"),
+    fullName: document.querySelector("#fullName"),
+    fatherName: document.querySelector("#fatherName"),
+    studentClass: document.querySelector("#studentClass"),
   };
 
-  // Result object
+    // Result Card Object 
   let result = {
-    math: Number(math.value),
-    science: Number(science.value),
-    computer: Number(computer.value),
-    pakistanStudies: Number(pakistanStudies.value),
-    english: Number(english.value),
-    urdu: Number(urdu.value),
-    remarks: remarks.value,
+    math: document.querySelector("#math"),
+    science: document.querySelector("#science"),
+    computer: document.querySelector("#computer"),
+    pakistanStudies: document.querySelector("#pakistanStudies"),
+    english: document.querySelector("#english"),
+    urdu: document.querySelector("#urdu"),
+    remarks: document.querySelector("#remarks"),
   };
 
   // Checking empty fields
   let isValid = true;
 
   // Roll No
-  if (student.rollNumber === "") {
-    rollNumberError.innerHTML = "Required Field is not filled.";
-    rollNumberError.style.color = "red";
+  if (student.rollNumber.value === "") {
+    document.querySelector("#rollNumberError").innerHTML = "Required Field is not filled.";
+    document.querySelector("#rollNumberError").style.color = "red";
     isValid = false;
   } else {
-    rollNumberError.innerHTML = "";
+    document.querySelector("#rollNumberError").innerHTML = "";
   }
 
-  // full name feild
-  if (student.fullName === "") {
-    fullNameError.innerHTML = "Required Field is not filled.";
-    fullNameError.style.color = "red";
+  // Full name field
+  if (student.fullName.value === "") {
+    document.querySelector("#fullNameError").innerHTML = "Required Field is not filled.";
+    document.querySelector("#fullNameError").style.color = "red";
     isValid = false;
   } else {
-    fullNameError.innerHTML = "";
+    document.querySelector("#fullNameError").innerHTML = "";
   }
 
-  // father name feild
-  if (student.fatherName === "") {
-    fatherNameError.innerHTML = "Required Field is not filled.";
-    fatherNameError.style.color = "red";
+  // Father name field
+  if (student.fatherName.value === "") {
+    document.querySelector("#fatherNameError").innerHTML = "Required Field is not filled.";
+    document.querySelector("#fatherNameError").style.color = "red";
     isValid = false;
   } else {
-    fatherNameError.innerHTML = "";
+    document.querySelector("#fatherNameError").innerHTML = "";
   }
 
-  // class feild
-  if (student.studentClass === "") {
-    studentClassError.innerHTML = "Required Field is not filled.";
-    studentClassError.style.color = "red";
+  // Class field
+  if (student.studentClass.value === "") {
+    document.querySelector("#studentClassError").innerHTML = "Required Field is not filled.";
+    document.querySelector("#studentClassError").style.color = "red";
     isValid = false;
   } else {
-    studentClassError.innerHTML = "";
+    document.querySelector("#studentClassError").innerHTML = "";
   }
 
-  // Math Feild
-  if (math.value === "") {
-    mathError.innerHTML = "Required Field is not filled.";
-    mathError.style.color = "red";
+  // Math Field 
+  if (result.math.value === "") {
+    document.querySelector("#mathError").innerHTML = "Required Field is not filled.";
+    document.querySelector("#mathError").style.color = "red";
     isValid = false;
-  } else if (result.math < 0 || result.math > 100) {
-    mathError.innerHTML = "Marks must be between 0 and 100.";
-    mathError.style.color = "red";
+  } else if (Number(result.math.value) < 0 || Number(result.math.value) > 100) {
+    document.querySelector("#mathError").innerHTML = "Marks must be between 0 and 100.";
+    document.querySelector("#mathError").style.color = "red";
     isValid = false;
   } else {
-    mathError.innerHTML = "";
+    document.querySelector("#mathError").innerHTML = "";
   }
 
-  // Science Feild
-  if (science.value === "") {
-    scienceError.innerHTML = "Required Field is not filled.";
-    scienceError.style.color = "red";
+  // Science Field
+  if (result.science.value === "") {
+    document.querySelector("#scienceError").innerHTML = "Required Field is not filled.";
+    document.querySelector("#scienceError").style.color = "red";
     isValid = false;
-  } else if (result.science < 0 || result.science > 100) {
-    scienceError.innerHTML = "Marks must be between 0 and 100.";
-    scienceError.style.color = "red";
+  } else if (Number(result.science.value) < 0 || Number(result.science.value) > 100) {
+    document.querySelector("#scienceError").innerHTML = "Marks must be between 0 and 100.";
+    document.querySelector("#scienceError").style.color = "red";
     isValid = false;
   } else {
-    scienceError.innerHTML = "";
+    document.querySelector("#scienceError").innerHTML = "";
   }
 
-  // Computer validation
-
-  if (computer.value === "") {
-    computerError.innerHTML = "Required Field is not filled.";
-    computerError.style.color = "red";
+  // Computer Field
+  if (result.computer.value === "") {
+    document.querySelector("#computerError").innerHTML = "Required Field is not filled.";
+    document.querySelector("#computerError").style.color = "red";
     isValid = false;
-  } else if (result.computer < 0 || result.computer > 100) {
-    computerError.innerHTML = "Marks must be between 0 and 100.";
-    computerError.style.color = "red";
+  } else if (Number(result.computer.value) < 0 || Number(result.computer.value) > 100) {
+    document.querySelector("#computerError").innerHTML = "Marks must be between 0 and 100.";
+    document.querySelector("#computerError").style.color = "red";
     isValid = false;
   } else {
-    computerError.innerHTML = "";
+    document.querySelector("#computerError").innerHTML = "";
   }
 
-  // Pakistan Studies validation
-
-  if (pakistanStudies.value === "") {
-    pakistanStudiesError.innerHTML = "Required Field is not filled.";
-    pakistanStudiesError.style.color = "red";
+  // Pakistan Studies Field
+  if (result.pakistanStudies.value === "") {
+    document.querySelector("#pakistanStudiesError").innerHTML = "Required Field is not filled.";
+    document.querySelector("#pakistanStudiesError").style.color = "red";
     isValid = false;
-  } else if (result.pakistanStudies < 0 || result.pakistanStudies > 100) {
-    pakistanStudiesError.innerHTML = "Marks must be between 0 and 100.";
-    pakistanStudiesError.style.color = "red";
+  } else if (Number(result.pakistanStudies.value) < 0 || Number(result.pakistanStudies.value) > 100) {
+    document.querySelector("#pakistanStudiesError").innerHTML = "Marks must be between 0 and 100.";
+    document.querySelector("#pakistanStudiesError").style.color = "red";
     isValid = false;
   } else {
-    pakistanStudiesError.innerHTML = "";
+    document.querySelector("#pakistanStudiesError").innerHTML = "";
   }
 
-  // English validation
-
-  if (english.value === "") {
-    englishError.innerHTML = "Required Field is not filled.";
-    englishError.style.color = "red";
+  // English Field
+  if (result.english.value === "") {
+    document.querySelector("#englishError").innerHTML = "Required Field is not filled.";
+    document.querySelector("#englishError").style.color = "red";
     isValid = false;
-  } else if (result.english < 0 || result.english > 100) {
-    englishError.innerHTML = "Marks must be between 0 and 100.";
-    englishError.style.color = "red";
+  } else if (Number(result.english.value) < 0 || Number(result.english.value) > 100) {
+    document.querySelector("#englishError").innerHTML = "Marks must be between 0 and 100.";
+    document.querySelector("#englishError").style.color = "red";
     isValid = false;
   } else {
-    englishError.innerHTML = "";
+    document.querySelector("#englishError").innerHTML = "";
   }
 
-  // Urdu validation
-  if (urdu.value === "") {
-    urduError.innerHTML = "Required Field is not filled.";
-    urduError.style.color = "red";
+  // Urdu Field
+  if (result.urdu.value === "") {
+    document.querySelector("#urduError").innerHTML = "Required Field is not filled.";
+    document.querySelector("#urduError").style.color = "red";
     isValid = false;
-  } else if (result.urdu < 0 || result.urdu > 100) {
-    urduError.innerHTML = "Marks must be between 0 and 100.";
-    urduError.style.color = "red";
+  } else if (Number(result.urdu.value) < 0 || Number(result.urdu.value) > 100) {
+    document.querySelector("#urduError").innerHTML = "Marks must be between 0 and 100.";
+    document.querySelector("#urduError").style.color = "red";
     isValid = false;
   } else {
-    urduError.innerHTML = "";
+    document.querySelector("#urduError").innerHTML = "";
   }
 
   // Remarks
-  if (result.remarks === "") {
-    remarksError.innerHTML = "Required Field is not filled.";
-    remarksError.style.color = "red";
+  if (result.remarks.value === "") {
+    document.querySelector("#remarksError").innerHTML = "Required Field is not filled.";
+    document.querySelector("#remarksError").style.color = "red";
     isValid = false;
   } else {
-    remarksError.innerHTML = "";
+    document.querySelector("#remarksError").innerHTML = "";
   }
 
-  // If the feilds are empty stop the function
+  // If the fields are empty stop the function
   if (isValid === false) {
     return;
   }
 
-  // Calculate total marks
-  result.totalMarks = result.math + result.science + result.computer + result.pakistanStudies + result.english + result.urdu;
+  // Calculate total marks 
+  result.totalMarks = Number(result.math.value) + Number(result.science.value) + Number(result.computer.value) + Number(result.pakistanStudies.value) + Number(result.english.value) + Number(result.urdu.value);
 
   // Calculate percentage
   result.percentage = Math.floor((result.totalMarks / 600) * 100);
@@ -177,36 +174,36 @@ function resultCardGeneration(event) {
   }
   result.grade = grade;
 
-  // Displaying Result Card
-  resultHeading.innerHTML = "Student Result Card";
-  studentInformation.innerHTML = "Student Information";
-  displayRollNumber.innerHTML = "Roll Number: " + student.rollNumber;
-  displayFullName.innerHTML = "Full Name: " + student.fullName;
-  displayFatherName.innerHTML = "Father Name: " + student.fatherName;
-  displayClass.innerHTML = "Class: " + student.studentClass;
-  subjectMarks.innerHTML = "Subject Marks";
-  displayMath.innerHTML = "Math: " + result.math;
-  displayScience.innerHTML = "Science: " + result.science;
-  displayComputer.innerHTML = "Computer: " + result.computer;
-  displayPakistanStudies.innerHTML = "Pakistan Studies: " + result.pakistanStudies;
-  displayEnglish.innerHTML = "English: " + result.english;
-  displayUrdu.innerHTML = "Urdu: " + result.urdu;
-  finalResult.innerHTML = "Result";
-  displayTotalMarks.innerHTML = "Total Marks: " + result.totalMarks + " / 600";
-  displayPercentage.innerHTML = "Percentage: " + result.percentage + "%";
-  displayGrade.innerHTML = "Grade: " + result.grade;
-  displayRemarks.innerHTML = "Remarks by Teacher: " + result.remarks;
+  // Displaying Result Card 
+  document.querySelector("#resultHeading").innerHTML = "Student Result Card";
+  document.querySelector("#studentInformation").innerHTML = "Student Information";
+  document.querySelector("#displayRollNumber").innerHTML = "Roll Number: " + student.rollNumber.value;
+  document.querySelector("#displayFullName").innerHTML = "Full Name: " + student.fullName.value;
+  document.querySelector("#displayFatherName").innerHTML = "Father Name: " + student.fatherName.value;
+  document.querySelector("#displayClass").innerHTML = "Class: " + student.studentClass.value;
+  document.querySelector("#subjectMarks").innerHTML = "Subject Marks";
+  document.querySelector("#displayMath").innerHTML = "Math: " + result.math.value;
+  document.querySelector("#displayScience").innerHTML = "Science: " + result.science.value;
+  document.querySelector("#displayComputer").innerHTML = "Computer: " + result.computer.value;
+  document.querySelector("#displayPakistanStudies").innerHTML = "Pakistan Studies: " + result.pakistanStudies.value;
+  document.querySelector("#displayEnglish").innerHTML = "English: " + result.english.value;
+  document.querySelector("#displayUrdu").innerHTML = "Urdu: " + result.urdu.value;
+  document.querySelector("#finalResult").innerHTML = "Result";
+  document.querySelector("#displayTotalMarks").innerHTML = "Total Marks: " + result.totalMarks + " / 600";
+  document.querySelector("#displayPercentage").innerHTML = "Percentage: " + result.percentage + "%";
+  document.querySelector("#displayGrade").innerHTML = "Grade: " + result.grade;
+  document.querySelector("#displayRemarks").innerHTML = "Remarks by Teacher: " + result.remarks.value;
   
-  // Cleaning Feilds
-  rollNumber.value = "";
-  fullName.value = "";
-  fatherName.value = "";
-  studentClass.value = "";
-  math.value = "";
-  science.value = "";
-  computer.value = "";
-  pakistanStudies.value = "";
-  english.value = "";
-  urdu.value = "";
-  remarks.value = "";
+  // Cleaning Fields 
+  student.rollNumber.value = "";
+  student.fullName.value = "";
+  student.fatherName.value = "";
+  student.studentClass.value = "";
+  result.math.value = "";
+  result.science.value = "";
+  result.computer.value = "";
+  result.pakistanStudies.value = "";
+  result.english.value = "";
+  result.urdu.value = "";
+  result.remarks.value = "";
 }
